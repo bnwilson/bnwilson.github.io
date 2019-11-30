7e8f52c0c4b0ab02d24da0c684a36512e8f0c006
bnwilson <bwilson.projects@gmail.com>
2019-11-29 23:21:28 -0500

    Added stying to Projects table

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,51 +1,49 @@
 import React from 'react';
 import '../styles/styles.css';
+import '../styles/project.css';
 
 const Projects = (props) => {
     return (
         <div className="main__content-wrapper">
             {props.children}
-            <table className="main__body-table">
-                <caption className="main__body-table-caption">My collection of publically-shared projects. Feel free to reach out to me with any questions, or raise a Github issue!
-                <br/><br/>
-                </caption>
-                <thead className="main__body-table-thead">
-                    <tr>
+            <p className="project-table__caption">My collection of publically-shared projects. Feel free to reach out to me with any questions, or raise a Github issue!
+            <br/><br/>
+            </p>
+            <table className="project-table">
+                <thead className="project-table__header">
+                    <tr className="project-table__row">
                         <th><h4>Project Name</h4></th>
                         <th><h4>Description</h4></th>
                         <th><h4>Tools Used</h4></th>
                         <th><h4>Link</h4></th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
+                <tbody className="project-table__body">
+                    <tr className="project-table__row">
                         <th>Highstreet Hospital</th>
                         <td>Console-based Hospital. Generates Hospital staff, patients, along with randomized names.</td>
                         <td>NodeJS, Javascript (ES6), readline-sync, https (REST call to name-generator)</td>
                         <td><a href="https://github.com/bnwilson/projects/tree/master/high-street-hospital">Github Link</a></td>
                     </tr>
-                    <tr>
+                    <tr className="project-table__row">
                         <th>Fortune Teller</th>
                         <td>Answer a series of questions and get your fortune read! The results are not always crystal clear.</td>
                         <td>NodeJS, Javascript (ES6), readline-sync</td>
                         <td><a href="https://github.com/bnwilson/projects/tree/master/fortune-teller">Github Link</a></td>
                     </tr>
-                    <tr>
+                    <tr className="project-table__row">
                         <th>Guessing Game</th>
                         <td>You get 3 tries to guess a number 1-10, if you don't guess the correct number in 3 tries you lose!</td>
                         <td>NodeJS, Javascript (ES6), readline-sync</td>
                         <td><a href="https://github.com/bnwilson/projects/tree/master/guessing-game">Github Link</a></td>
                     </tr>
-                </tbody>
-                <tfoot>
-                    <tr>
+                    <tr className="project-table__row">
                         <th>Move Mac Screenshots</th>
                         <td>Script which moves my MacOS Desktop screenshots to a screenshot folder.</td>
                         <td>Python</td>
                         <td><a href="https://github.com/bnwilson/projects/tree/master/move-mac-screenshots">Github Link</a></td>
                     </tr>
-                </tfoot>
-                
+                </tbody>
 
             </table>
         </div>
